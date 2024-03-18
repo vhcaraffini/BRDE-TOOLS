538b28d5c09bd323a31fd933c1f6f2e3c0d68c52
Ichiruky <marcuskotviski@gmail.com>
2024-03-18 14:54:19 -0300

    Ajustando a função de baixa BRBANK

--- termos/baixa_brbank.py
+++ termos/baixa_brbank.py
@@ -10,7 +10,7 @@ import tkinter as tk
 from tkinter import filedialog
 import pandas as pd
 
-TIMER = 0.5
+TIMER = 0.75
 
 def alterando_situacao_contrato():
     # Abrindo Excel
@@ -20,30 +20,10 @@ def alterando_situacao_contrato():
     CAMINHO_ARQUIVO = filedialog.askopenfilename(initialdir="/", title="Selecione um arquivo", filetypes=(("Arquivos do Excel", "*.xlsx"), ("Todos os arquivos", "*.*")))
     df = pd.read_excel(CAMINHO_ARQUIVO, sheet_name='Planilha1')
 
-    for i, mutuario in enumerate(df['Número do contrato']):
-        ...
-    # # Listas
-    # mutuarios = []
-    # valores = []
-    # datas = []
-
-    # Pegando valores do excel e adicionando a lista
-    # for row in sheet.iter_rows(values_only=True):
-    #     if row[0] == 'EMPRESA':
-    #         continue
-
-    #     elif row[0] == 'TOTAL - TARIFA DE CADASTRO':
-    #         break
-
-    #     mutuarios.append(row[0])
-    #     valores.append(str(row[1]) + '00')
-    #     datas.append(row[2])
-
-    # mutuarios
-    for n in range(1):
-        # mutuario = mutuarios[n].strip()
-        # valor = valores[n]
-        # data = datas[n].strftime('%d/%m/%Y')
+    for i, contrato in enumerate(df['Contrato']):
+        cgc = str(df.loc[i, 'CPF/CNPJ'])
+        numero_termo = str(df.loc[i, 'nrTQ'])
+        data_termo = df.loc[i, 'dtTQ']
 
         # Acessando o site
         driver = webdriver.Chrome()
@@ -55,8 +35,8 @@ def alterando_situacao_contrato():
         sleep(TIMER)
 
         # Pesquisando pelo nome
-        pesquisando_por_nome = driver.find_element(By.ID, 'nomeCnpjCpfModal')
-        pesquisando_por_nome.send_keys('Enio Jose Seganfredo')
+        pesquisando_por_cgc = driver.find_element(By.ID, 'nomeCnpjCpfModal')
+        pesquisando_por_cgc.send_keys(str(cgc))
         sleep(TIMER)
         pesquisando = driver.find_element(By.XPATH, '//*[@id="searchPessoaBtn"]/span')
         pesquisando.click()
@@ -71,28 +51,61 @@ def alterando_situacao_contrato():
         dand_ok.click()
         sleep(TIMER)
 
+        n = 0
+        contrato_site = ''
+        while str(contrato_site) != str(contrato):
+            n += 1
+            try:
+                encontrando_contrato = driver.find_element(By.XPATH, f'/html/body/div/div/div/div/form/fieldset/section[2]/div/table/tbody/tr/td[1]')
+                contrato_site = encontrando_contrato.text
+            except:
+                encontrando_contrato = driver.find_element(By.XPATH, f'/html/body/div/div/div/div/form/fieldset/section[2]/div/table/tbody/tr[{n}]/td[1]')
+                contrato_site = encontrando_contrato.text
+
+            print(contrato_site)
+            print(contrato)
         # Entrando no lapiz
-        lapiz_edicao = driver.find_element(By.XPATH, '//*[@id="quitacaoContratosTable"]/tbody/tr/td[9]/div/a[1]/span')
-        lapiz_edicao.click()
+        try:
+            lapiz_edicao = driver.find_element(By.XPATH, '/html/body/div/div/div/div/form/fieldset/section[2]/div/table/tbody/tr/td[9]/div/a[1]/span')
+            lapiz_edicao.click()
+        except:
+            lapiz_edicao = driver.find_element(By.XPATH, f'/html/body/div/div/div/div/form/fieldset/section[2]/div/table/tbody/tr[{n}]/td[9]/div/a[1]/span')
+            lapiz_edicao.click()
         sleep(TIMER)
 
         # Modificando situação
-        modifica_situacao = driver.find_element(By.XPATH, "/html/body/div/div/div/div/form/fieldset/section[2]/div/table/tbody/tr/td[8]/span/span[1]/span")
-        modifica_situacao.click()
-        modifica_situacao.send_keys(Keys.ARROW_DOWN)
-        modifica_situacao.send_keys(Keys.ENTER)
+        try:
+            modifica_situacao = driver.find_element(By.XPATH, "/html/body/div/div/div/div/form/fieldset/section[2]/div/table/tbody/tr/td[8]/span/span[1]/span")
+            modifica_situacao.click()
+            modifica_situacao.send_keys(Keys.ARROW_DOWN)
+            modifica_situacao.send_keys(Keys.ENTER)
+        except:
+            modifica_situacao = driver.find_element(By.XPATH, f"/html/body/div/div/div/div/form/fieldset/section[2]/div/table/tbody/tr[{n}]/td[8]/span/span[1]/span")
+            modifica_situacao.click()
+            modifica_situacao.send_keys(Keys.ARROW_DOWN)
+            modifica_situacao.send_keys(Keys.ENTER)
         sleep(TIMER)
 
         # Preenche numero do termo
-        preenche_numero_termo = driver.find_element(By.XPATH, '/html/body/div/div/div/div/form/fieldset/section[2]/div/table/tbody/tr/td[6]/input')
-        preenche_numero_termo.send_keys('12323')
-
+        try:
+            preenche_numero_termo = driver.find_element(By.XPATH, '/html/body/div/div/div/div/form/fieldset/section[2]/div/table/tbody/tr/td[6]/input')
+            preenche_numero_termo.send_keys(numero_termo)
+        except:
+            preenche_numero_termo = driver.find_element(By.XPATH, f'/html/body/div/div/div/div/form/fieldset/section[2]/div/table/tbody/tr[{n}]/td[6]/input')
+            preenche_numero_termo.send_keys(numero_termo)
+        sleep(TIMER)
+        
         # Preenche data do termo
-        preenche_data_termo = driver.find_element(By.XPATH, '/html/body/div/div/div/div/form/fieldset/section[2]/div/table/tbody/tr/td[6]/input')
-        preenche_data_termo.send_keys('12/03/2023')
+        try:
+            preenche_data_termo = driver.find_element(By.XPATH, '/html/body/div/div/div/div/form/fieldset/section[2]/div/table/tbody/tr/td[7]/input')
+            preenche_data_termo.send_keys(data_termo)
+        except:
+            preenche_data_termo = driver.find_element(By.XPATH, f'/html/body/div/div/div/div/form/fieldset/section[2]/div/table/tbody/tr[{n}]/td[7]/input')
+            preenche_data_termo.send_keys(data_termo)
+        sleep(TIMER)
 
         # Encontra o Salvar
-        ending = driver.find_element(By.XPATH, '/html/body/div/div/div/div/form/fieldset/section[2]/div/table/tbody/tr/td[9]/div/a[2]/span')
+        # ending = driver.find_element(By.XPATH, '/html/body/div/div/div/div/form/fieldset/section[2]/div/table/tbody/tr/td[9]/div/a[2]/span')
         # ending.click()
 
         # # Salva
