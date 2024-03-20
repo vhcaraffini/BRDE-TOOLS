1d0330e0a81ef1ac9f9b02ad114148e7703e6ad9
Ichiruky <marcuskotviski@gmail.com>
2024-03-20 10:01:06 -0300

    Adicionando botão Alterar SItuação

--- window_termos.py
+++ window_termos.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QVBoxLayout, QWidget, QLabel, QPushButton
 from termos.separador_de_termos import separa_termos
+from termos.baixa_brbank import alterando_situacao_contrato
 
 class window_termos(QWidget):
     def __init__(self, main_window):
@@ -15,6 +16,10 @@ class window_termos(QWidget):
         self.separar_termos.clicked.connect(self.separador_de_termos)
         self.layout.addWidget(self.separar_termos)
 
+        self.alterar_situacao_contrato = QPushButton("Alterar Situação BRBANK")
+        self.alterar_situacao_contrato.clicked.connect(self.alterador_situacao_contrato)
+        self.layout.addWidget(self.alterar_situacao_contrato)
+
         self.layout.addWidget(QPushButton("Voltar", clicked=self.return_main_window))
 
         self.main_window = main_window
@@ -24,3 +29,6 @@ class window_termos(QWidget):
 
     def separador_de_termos(self):
         separa_termos()
+
+    def alterador_situacao_contrato(self):
+        alterando_situacao_contrato()
